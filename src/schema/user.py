5e98e0bf0cda4433e518630831fce5ea568df9d6
NJawlaJosh <nishant.jawla@joshtechnologygroup.com>
2022-10-11 11:29:22 +0530

    upgrade:added constants for easy replacements

--- src/schema/user.py
+++ src/schema/user.py
@@ -2,6 +2,8 @@ from marshmallow import fields, validate
 
 from src.schema import ma
 from src.constants.india_states import indian_states
+from src.schema.restaurant import RestaurantSchema
+from src.constants.user_constants import NAME_LENGTH_MIN,  NAME_LENGTH_MAX, PASSWORD_LENGTH_MIN,  PASSWORD_LENGTH_MAX,  CITY_LENGTH_MIN,  CITY_LENGTH_MAX, RESTAURANT_FIELDS,  STATE_LENGTH_MIN,  STATE_LENGTH_MAX,  ZIPCODE_MIN,  ZIPCODE_MAX,  BALANCE_MIN,  CITY_REGEX,  NAME_LENGTH_ERROR,  PASSWORD_LENGTH_ERROR,  CITY_LENGTH_ERROR,  CITY_REGEX_ERROR,  STATE_LENGTH_ERROR,  STATE_ONEOF_ERROR,  ZIPCODE_LENGTH_ERROR,  BALANCE_MIN_ERROR
 
 
 class UserSchema(ma.SQLAlchemySchema):
@@ -10,26 +12,27 @@ class UserSchema(ma.SQLAlchemySchema):
         primary_key = '_id'
     _id = fields.Integer(dump_only=True)
     name = fields.String(
-        required=True, validate=validate.And(validate.Length(min=1, max=100, error="Name must be between 1 and 100 characters long"), validate.Regexp(r'^([a-zA-Z]+\s)*[a-zA-Z]+$', error="Name must contain only letters and spaces"))
+        required=True, validate=validate.Length(min=NAME_LENGTH_MIN, max=NAME_LENGTH_MAX, error=NAME_LENGTH_ERROR)
     )
     email = fields.Email(required=True, validate=validate.Email())
     password = fields.String(
-        required=True, validate=validate.Length(min=5, max=300, error="Password must be between 5 and 300 characters long")
+        required=True, validate=validate.Length(min=PASSWORD_LENGTH_MIN, max=PASSWORD_LENGTH_MAX, error=PASSWORD_LENGTH_ERROR)
     )
     city = fields.String(
-        required=True, validate=validate.And(validate.Length(min=2, max=100, error="City must be between 2 and 100 characters long"), validate.Regexp(r'^([a-zA-Z]+\s)*[a-zA-Z]+$', error="City must contain only letters and spaces"))
+        required=True, validate=validate.And(validate.Length(min=CITY_LENGTH_MIN, max=CITY_LENGTH_MAX, error=CITY_LENGTH_ERROR), validate.Regexp(CITY_REGEX, error=CITY_REGEX_ERROR))
     )
     state = fields.String(
-        required=True, validate=validate.And(validate.Length(min=2, max=100, error="State must be between 2 and 100 characters long"), validate.OneOf(indian_states, error="State must be a valid Indian state"))
+        required=True, validate=validate.And(validate.Length(min=STATE_LENGTH_MIN, max=STATE_LENGTH_MAX, error=STATE_LENGTH_ERROR), validate.OneOf(indian_states, error=STATE_ONEOF_ERROR))
     )
     zipcode = fields.Integer(required=True, validate=validate.Range(
-        min=10000, max=99999, error="Zipcode must be 5 digits long")
+        min=ZIPCODE_MIN, max=ZIPCODE_MAX, error=ZIPCODE_LENGTH_ERROR)
     )
     balance = fields.Float(required=True, validate=validate.Range(
-        min=0, error="Balance must be greater than or equal to 0")
+        min=BALANCE_MIN, error=BALANCE_MIN_ERROR)
     )
-    restaurants = fields.Nested('RestaurantSchema', many=True, only=[
-                                '_id', 'name'], dump_only=True)
+    restaurants = fields.Nested(
+        'RestaurantSchema', many=True, only=RESTAURANT_FIELDS, dump_only=True
+    )
     created_at = fields.DateTime(dump_only=True)
     updated_at = fields.DateTime(dump_only=True)
     active = fields.Boolean()
