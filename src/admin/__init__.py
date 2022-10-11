302bb4334ffffb68792e9dcc30f2241da5f40265
NJawlaJosh <nishant.jawla@joshtechnologygroup.com>
2022-10-12 02:03:31 +0530

    upgrade: added fields locks for admin

--- src/admin/__init__.py
+++ src/admin/__init__.py
@@ -1,5 +1,6 @@
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from src.admin.models import AdminUserView
 
 from src.models import db
 from src.models.user import User
@@ -8,6 +9,6 @@ from src.models.restaurant import Restaurant
 
 def create_admin(app):
     admin = Admin(app, name='Admin', template_mode='bootstrap3')
-    admin.add_view(ModelView(User, db.session))
+    admin.add_view(AdminUserView(User, db.session))
     admin.add_view(ModelView(Restaurant, db.session))
     return admin
